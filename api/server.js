fcbce956be626a8683882653b914712175dfa943
Jonathan Dorety <kyullog@gmail.com>
2019-04-02 22:20:03 -0400

    create and implement userRouter with GET method

--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const authRouter = require("../auth/authRouter.js");
+const userRouter = require("../users/userRouter.js");
 
 const server = express();
 server.use(cors(), helmet());
 server.use(express.json());
 
 server.use("/api/auth", authRouter);
+server.use("/api/users", userRouter);
 
 module.exports = server;
