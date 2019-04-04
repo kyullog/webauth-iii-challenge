42e5693dc5191f4cc703e68b9c2e540bd9bef3c6
Jonathan Dorety <kyullog@gmail.com>
2019-04-03 20:21:00 -0400

    Create react app and add router and bulma framework

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
